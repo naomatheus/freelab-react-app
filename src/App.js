8aa17af3b343dc106a77475c87f7a929d2047630
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-05-14 13:05:56 -0500

    added Login container, githubjobs container, and login registers changes in the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import MainContainer from './MainContainer'
+import MainContainer from './MainContainer';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       App container
       <MainContainer />
+      <Login />
     </div>
   );
 }
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,1 +1,58 @@
-index.js
+import React, { Component } from 'react';
+
+class Login extends Component {
+	constructor(){
+		super()
+
+		this.state = {
+			username: '',
+			password: ''
+		};
+	}
+
+	handleSubmit = (e) => {
+		console.log(e);
+	};
+
+	handleChange = (e) => {
+		
+		console.log(e.target.value, '<-- this is event');
+		this.setState({
+			// computed property syntax
+			[e.target.name] : e.target.value
+		})
+
+	}
+
+	render(){
+
+		return (
+
+			<div>
+				This is the Login container
+				<form onSubmit={this.handleSubmit}>
+					<input 
+						type='text' 
+						placeholder='username'
+						onChange={this.handleChange}
+					/>
+					<br/>
+
+					<input 
+						type='password'
+						placeholder='password'
+						onChange={this.handleChange}
+					/> 
+					<br/>
+
+					<button type='submit'>Login</button>
+				</form>
+			</div>
+		)
+	}
+
+
+}
+
+
+export default Login;
